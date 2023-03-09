1043d17efb620cd744421b882a988faa73c07df3
Muktanshu Mishra <muktanshu@Mishras-M2-Air.local>
2023-03-09 14:33:23 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,43 +1,443 @@
-import { AuthProvider, CHAIN } from '@arcana/auth';
+const { AuthProvider, CHAIN } = window.arcana.auth;
 
-const connectArcana = async () => {
-    if(!arcanaProvider) {
-      const auth = new AuthProvider(`${process.env.App_ARCANA_APP_ID}`, {
-        position: 'right', // defaults to right
-        theme: 'dark', // defaults to dark
-        alwaysVisible: false,
-        chainConfig: {
-          chainId: CHAIN.POLYGON_MUMBAI_TESTNET,
-          rpcUrl: 'https://rpc-mumbai.maticvigil.com/',
+let provider;
+const auth = new AuthProvider("9b74fe1913101eec12b06fb9b8fa579ced8b91f9", {
+  network: "dev",
+  alwaysVisible: true,
+  position: "right",
+  theme: "dark"
+});
+provider = auth.provider;
+setHooks();
+
+console.log("Before load");
+window.onload = async () => {
+  try {
+    console.time("auth_init");
+    await auth.init();
+    console.timeEnd("auth_init");
+    console.log("Init auth complete!");
+  } catch (e) {
+    console.log({ e });
+  }
+};
+
+const reqElement = document.getElementById("request");
+const resElement = document.getElementById("result");
+const accElement = document.getElementById("account");
+
+function setRequest(value) {
+  reqElement.innerText = value;
+  setResult("-");
+}
+
+function setResult(value) {
+  resElement.innerText = value;
+  if (value !== "-") {
+    resElement.title = value;
+  }
+}
+
+function setAccount(value) {
+  accElement.innerText = value;
+}
+
+// get from eth_accounts
+let from = "";
+
+function setHooks() {
+  provider.on("connect", async (params) => {
+    console.log({ type: "connect", params: params });
+  });
+  provider.on("accountsChanged", (params) => {
+    console.log({ type: "accountsChanged", params: params });
+  });
+  provider.on("chainChanged", async (params) => {
+    console.log({ type: "chainChanged", params: params });
+  });
+}
+
+async function logout() {
+  console.log("Requesting logout");
+  try {
+    setRequest("Logout");
+    await auth.logout();
+    setResult("Logged out");
+  } catch (e) {
+    console.log({ e });
+  }
+}
+
+async function addChain() {
+  try {
+    setRequest("wallet_addEthereumChain");
+    const response = await provider.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          chainId: "8081",
+          chainName: "Shardeum",
+          blockExplorerUrls: ["https://explorer-liberty20.shardeum.org/"],
+          rpcUrls: ["https://liberty20.shardeum.org/"],
+          nativeCurrency: {
+            symbol: "	SHM"
+          }
         }
-      })
+      ]
+    });
+    setResult(response);
+  } catch (e) {
+    console.log({ e });
+  }
+}
 
-      try {
-        await auth.init()
-        await auth.connect()
-      } catch (e) {
-        console.log("Arcana Error: ", e);
-      }
-      arcanaProvider = auth;
-      setIsArcanaLogin(true)
-    } else {
-      const connected = await arcanaProvider.isLoggedIn()
-      if(connected) {
-        const arcanaUserInfo = await arcanaProvider.getUser();
-        if(arcanaUserInfo) {
-          const arcanaPublicKey = arcanaUserInfo.publicKey;
-          const hash = await arcanaProvider.provider.request({
-            method: 'eth_sendTransaction',
-              params: [{
-                arcanaPublicKey,
-                to: "0xZABABF507273311197d4D14de33C8C9F833C873B",
-                value: 0,
-            },],
-          })
-          console.log({ hash })
+async function switchChain() {
+  try {
+    setRequest("wallet_switchEthereumChain");
+    const response = await provider.request({
+      method: "wallet_switchEthereumChain",
+      params: [
+        {
+          chainId: "8081"
         }
+      ]
+    });
+    setResult(response);
+  } catch (e) {
+    console.log({ e });
+  }
+}
 
+let contractAddress;
+
+async function addToken() {
+  try {
+    setRequest("wallet_watchAsset");
+    const response = await provider.request({
+      method: "wallet_watchAsset",
+      params: {
+        type: "ERC20",
+        options: {
+          address:
+            contractAddress || "0xB983E01458529665007fF7E0CDdeCDB74B967Eb6",
+          symbol: "FOO",
+          decimals: 18,
+          image: "https://foo.io/token-image.svg"
+        }
       }
-    }
+    });
+    setResult(response);
+    // await provider.request({
+    //   method: "wallet_watchAsset",
+    //   params: {
+    //     type: "ERC721",
+    //     options: {
+    //       address: "0xdd03355f86270c4fc3bfa2d99c12540764f3786a",
+    //       token_id:
+    //         "57934446494755575015343905315045939479642979767544130360694570163757472760388",
+    //       name: "bayc"
+    //     }
+    //   }
+    // });
+  } catch (e) {
+    console.log({ e });
+  }
+}
 
-  };
+async function getAccounts() {
+  console.log("Requesting accounts");
+  try {
+    setRequest("eth_accounts");
+    const accounts = await provider.request({ method: "eth_accounts" });
+    console.log({ accounts });
+    from = accounts[0];
+    setAccount(accounts);
+    setResult(JSON.stringify(accounts));
+  } catch (e) {
+    console.log({ e });
+  }
+}
+
+async function requestAccounts() {
+  console.log("Requesting accounts");
+  try {
+    setRequest("eth_requestAccounts");
+    const accounts = await provider.request({ method: "eth_requestAccounts" });
+    console.log({ accounts });
+    from = accounts[0];
+    setAccount(from);
+    setResult(JSON.stringify(accounts));
+  } catch (e) {
+    console.log({ e });
+  }
+}
+
+async function sign() {
+  console.log("Requesting signature");
+  setRequest("eth_sign");
+  if (!from) {
+    return setResult("ERROR: Click get accounts to continue");
+  }
+  const signature = await provider.request({
+    method: "eth_sign",
+    params: [from, "some_random_data"]
+  });
+  setResult(signature);
+  console.log({ signature });
+}
+
+async function connect() {
+  console.log("Requesting connect wallet");
+  setRequest("Connect Wallet");
+  try {
+    const provider = await auth.connect();
+    console.log({ provider });
+    setResult("Logged in using connect_wallet");
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+async function socialLogin() {
+  console.log("Requesting login");
+  setRequest("Social Login");
+  await auth.loginWithSocial("google");
+  setResult("Logged in using social login");
+}
+
+async function linkLogin() {
+  console.log("Requesting passwordlesslogin");
+  setRequest("Passwordless Login");
+  await auth.loginWithLink("shrinath@newfang.io");
+  setResult("Logged in using email");
+}
+
+async function personalSign() {
+  console.log("Requesting personal signature");
+  setRequest("personal_sign");
+  if (!from) {
+    return setResult("ERROR: Click get accounts to continue");
+  }
+  const personalSign = await provider.request({
+    method: "personal_sign",
+    params: ["0", from]
+  });
+
+  setResult(personalSign);
+  console.log({ personalSign });
+}
+
+let plaintext = "I am a plaintext!";
+let ciphertext;
+let publicKey;
+
+async function encrypt() {
+  // console.log('Doing encryption')
+  // setRequest('encrypt')
+  // const c = await encryptWithPublicKey({
+  //   publicKey,
+  //   message: plaintext,
+  // })
+  // setResult(c)
+  // console.log({ ciphertext: c })
+  // ciphertext = c
+}
+
+async function ethDecrypt() {
+  console.log("Requesting decryption");
+  setRequest("eth_decrypt");
+  if (!from) {
+    return setResult("ERROR: Click get accounts to continue");
+  }
+  if (!ciphertext) {
+    return setResult("No ciphertext, ignoring event");
+  }
+  const plaintext = await provider.request({
+    method: "eth_decrypt",
+    params: [ciphertext, from]
+  });
+  console.log({ plaintext });
+  setResult(plaintext);
+}
+
+async function sendTransaction() {
+  setRequest("eth_sendTransaction");
+  if (!from) {
+    return setResult("ERROR: Click get accounts to continue");
+  }
+  const hash = await provider.request({
+    method: "eth_sendTransaction",
+    params: [
+      {
+        from,
+        gasPrice: 0,
+        to: "0xE28F01Cf69f27Ee17e552bFDFB7ff301ca07e780",
+        value: "0x01"
+      }
+    ]
+  });
+  setResult(hash);
+
+  console.log({ hash });
+}
+async function signTransaction() {
+  setRequest("eth_signTransaction");
+  if (!from) {
+    return setResult("ERROR: Click get accounts to continue");
+  }
+  const sig = await provider.request({
+    method: "eth_signTransaction",
+    params: [
+      {
+        from,
+        gasPrice: 0,
+        to: "0xE28F01Cf69f27Ee17e552bFDFB7ff301ca07e780",
+        value: "0x0de0b6b3a7640000"
+      }
+    ]
+  });
+  setResult(sig);
+
+  console.log({ sig });
+}
+
+async function getSelfPublicKey() {
+  console.log("Requesting public key");
+  setRequest("eth_getEncryptionPublicKey");
+  if (!from) {
+    return setResult("ERROR: Click get accounts to continue");
+  }
+  const pk = await provider.request({
+    method: "eth_getEncryptionPublicKey",
+    params: [from]
+  });
+  console.log({ pk });
+  setResult(pk);
+  publicKey = pk;
+}
+
+async function getPublicKey() {
+  setRequest("getPublicKey");
+
+  console.log("Requesting others public key");
+  const pk = await auth.getPublicKey("makyl@newfang.io");
+  setResult(pk);
+
+  console.log({ pk });
+}
+
+async function signTyped() {
+  setRequest("eth_signTypedData_v4");
+  if (!from) {
+    return setResult("ERROR: Click get accounts to continue");
+  }
+  console.log("Requesting typed signature");
+  const typedSign = await provider.request({
+    method: "eth_signTypedData_v4",
+    params: [from, msgParams]
+  });
+
+  setResult(typedSign);
+
+  console.log({ typedSign });
+}
+
+const msgParams = JSON.stringify({
+  domain: {
+    // Defining the chain aka Rinkeby testnet or Ethereum Main Net
+    chainId: 1,
+    // Give a user friendly name to the specific contract you are signing for.
+    name: "Ether Mail",
+    // If name isn't enough add verifying contract to make sure you are establishing contracts with the proper entity
+    verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+    // Just let's you know the latest version. Definitely make sure the field name is correct.
+    version: "1"
+  },
+
+  // Defining the message signing data content.
+  message: {
+    /*
+     - Anything you want. Just a JSON Blob that encodes the data you want to send
+     - No required fields
+     - This is DApp Specific
+     - Be as explicit as possible when building out the message schema.
+    */
+    contents: "Hello, Bob!",
+    attachedMoneyInEth: 4.2,
+    from: {
+      name: "Cow",
+      wallets: [
+        "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+        "0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF"
+      ]
+    },
+    to: [
+      {
+        name: "Bob",
+        wallets: [
+          "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+          "0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57",
+          "0xB0B0b0b0b0b0B000000000000000000000000000"
+        ]
+      }
+    ]
+  },
+  // Refers to the keys of the *types* object below.
+  primaryType: "Mail",
+  types: {
+    // TODO: Clarify if EIP712Domain refers to the domain the contract is hosted on
+    EIP712Domain: [
+      { name: "name", type: "string" },
+      { name: "version", type: "string" },
+      { name: "chainId", type: "uint256" },
+      { name: "verifyingContract", type: "address" }
+    ],
+    // Not an EIP712Domain definition
+    Group: [
+      { name: "name", type: "string" },
+      { name: "members", type: "Person[]" }
+    ],
+    // Refer to PrimaryType
+    Mail: [
+      { name: "from", type: "Person" },
+      { name: "to", type: "Person[]" },
+      { name: "contents", type: "string" }
+    ],
+    // Not an EIP712Domain definition
+    Person: [
+      { name: "name", type: "string" },
+      { name: "wallets", type: "address[]" }
+    ]
+  }
+});
+
+async function deploy() {
+  setRequest("Ethers ContractFactory Deploy method");
+  const { abi20, byteCode } = await (await fetch("./artifact-lib.json")).json();
+  const web3pro = new window.ethers.providers.Web3Provider(provider);
+  const signer = await web3pro.getSigner();
+  const cFactory = new window.ethers.ContractFactory(abi20, byteCode, signer);
+  const contract = await cFactory.deploy(20000);
+  contractAddress = contract.address;
+  setResult(contract.address);
+}
+
+async function interact() {
+  setRequest("Ethers Contract Transfer method");
+  if (!contractAddress) {
+    return setResult("ERROR: Deploy Contract to interact with it");
+  }
+  const { abi20 } = await (await fetch("./artifact-lib.json")).json();
+  const web3pro = new window.ethers.providers.Web3Provider(provider);
+  const signer = await web3pro.getSigner();
+  const contract = new window.ethers.Contract(contractAddress, abi20, signer);
+  let tx = await contract.transfer(
+    "0xd5485e03893854078C4d825C7794162643C38158",
+    1
+  );
+  await tx.wait();
+  console.log(await contract.balanceOf(await signer.getAddress()));
+  setResult(
+    "Balance: " + (await contract.balanceOf(await signer.getAddress()))
+  );
+}
